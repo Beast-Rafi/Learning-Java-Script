828a78ad0a4e3e0b39aab11658e0f2324705fb70
Fahim Shakil Rafi <98203588+Beast-Rafi@users.noreply.github.com>
2023-03-29 11:57:30 +0600

    Add files via upload

--- Learning JS/Black Jak game/index.js
+++ Learning JS/Black Jak game/index.js
@@ -1,15 +1,21 @@
 let firstCard = 2
 let secondCard = 11
 let hasBlackJack = false
+let cards = [firstCard , secondCard]
 let isAlive = true
 let message
 let sum = firstCard + secondCard
 let msgEl = document.getElementById("msg-El")
-//let sumEl = document.getElementById("sum-El")
-let sumEl = document.querySelector("#sum-El")
+let sumEl = document.getElementById("sum-El")
+let cardsEl = document.getElementById("cards-El")
 
-function start() {
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
     sumEl.textContent += sum
+    cardsEl.textContent += " " + cards[0] + " " + cards[1]
 
     if (sum <= 20) {
         message = "Do you want to roll a new card?"
@@ -25,3 +31,10 @@ function start() {
     msgEl.textContent = message
 }
 
+function newCard() {
+    let thirdCard = 4
+    cards.push(thirdCard)
+    sum = sum + thirdCard + " "
+    cardsEl.textContent += sum
+    renderGame()
+}
